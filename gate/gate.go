3096bcd3deabbbfa9350ccd592366554a031495c
freedian <freedian@qq.com>
2015-11-30 23:50:49 +0800

    ggs.sample.env

---
+++ gate/gate.go
@@ -0,0 +1,18 @@
+package gate
+
+var Service = new(Gate)
+
+type Gate struct {
+	
+}
+
+func (gate *Gate) OnInit() {
+}
+
+func (gate *Gate) Run(closeSig chan bool) {
+
+}
+
+func (gate *Gate) OnDestroy() {
+	
+}
